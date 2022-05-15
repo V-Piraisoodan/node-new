b204dbe2a3cdea171beb5cdd6d5b40552796f5af
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-05-15 18:52:27 +0530

    create a model

---
+++ Model/product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema (
+    {
+        productName : {
+            type : String,
+            minlength : 5,
+            maxlength : 25,
+            required : true,
+            enum : ['laptop','mobile','television']
+        },
+        quantity : {
+            type : Number,
+            required : true
+        },
+        Price : {
+            type : Number,
+            required : true
+        },
+        description : {
+            type : String,
+            minlength : 5,
+            maxlength : 250,
+            required : false
+        },
+        userQuantity : {
+            type : Number,
+            required : true
+        }
+    }
+)
+
+//  ippothu mela ulla schema vai vaithu oru model create pannanum
+
+const Product = mongoose.model('Product',productSchema);
+module.exports = Product;
