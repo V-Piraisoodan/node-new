a70b18c59a90dd0e8c0d045ab6aeffb1dfaa12c4
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-05-17 23:54:25 +0530

    router edit

--- router/product.js
+++ router/product.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const productModule = require("../module/product");
 
 router.post('/create', productModule.createProduct);
-// router.get('/get/:employeeId', employeeModule.getEmployees); //to get only one employee details
-// router.post('/create', employeeModule.createEmployees);
-// router.put('/update/:employeeId',employeeModule.updateEmployees);
+router.get('/get', productModule.getProducts); //to get only one employee details
+router.put('/update/:productId',productModule.updateProducts);
+router.delete('/remove/:productId', productModule.deleteProducts);
 // router.put('/update/:employeeId/find',employeeModule.findAndUpdateEmployees);
-// router.delete('/remove/:employeeId', employeeModule.deleteEmployees);
+
 
 module.exports = router;
